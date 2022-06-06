8b7f95e5b1589e2bb15b1758b92721cbf0700da8
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-06-07 00:10:18 +0530

    new

---
+++ GRAPHS/Flood-fill.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(int i,int j,int initial,int final,vector<vector<int>>& image){
+        int r=image.size();
+        int c=image[0].size();
+        if(i<0 || j<0 || i>=r || j>=c)
+            return;
+        if(initial != image[i][j])
+            return;
+        
+       image[i][j]=final;
+        dfs(i,j+1,initial,final,image);
+        dfs(i,j-1,initial,final,image);
+        dfs(i+1,j,initial,final,image);
+        dfs(i-1,j,initial,final,image);
+         
+        
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int final) {
+        int initial=image[sr][sc];
+        if(initial!=final)
+         dfs(sr,sc,initial,final,image);
+        return image;
+    }
+};
